049056fa3e9506fa4d6499df9a4c911f9fe5ae5c
Pat <pyoon99@gmail.com>
2018-07-14 22:30:28 -0400

    added parameter test function

--- ports/esp32/raw_packet.c
+++ ports/esp32/raw_packet.c
@@ -11,9 +11,16 @@ STATIC mp_obj_t raw_hello(void){
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(raw_hello_obj, raw_hello);
 
+STATIC mp_obj_t saySomething(mp_obj_t something){
+  printf("You said: %s\n", mp_obj_str_get_str(something));
+  return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(saySomething_obj, saySomething);
+
 STATIC const mp_map_elem_t rawpacket_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_rawpacket) },
     {MP_OBJ_NEW_QSTR(MP_QSTR_hey), (mp_obj_t)&raw_hello_obj},
+    {MP_OBJ_NEW_QSTR(MP_QSTR_say_something), (mp_obj_t)&saySomething_obj},
 };
 
 STATIC MP_DEFINE_CONST_DICT (
